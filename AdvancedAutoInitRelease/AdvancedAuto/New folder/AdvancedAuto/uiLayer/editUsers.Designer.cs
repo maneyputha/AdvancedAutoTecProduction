74f2f736b93918643b31eec11b1a2463551f8ab9
manendra <manendraranathunga@gmail.com>
2015-08-23 02:38:35 +0530

    user update and delete completed

---
+++ AdvancedAutoInitRelease/AdvancedAuto/New folder/AdvancedAuto/uiLayer/editUsers.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace uiLayer
+{
+    partial class editUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userNameVerify = new System.Windows.Forms.Label();
+            this.roleVerify = new System.Windows.Forms.Label();
+            this.passwordVerify = new System.Windows.Forms.Label();
+            this.nicVerify = new System.Windows.Forms.Label();
+            this.lnameVerify = new System.Windows.Forms.Label();
+            this.fnameVerify = new System.Windows.Forms.Label();
+            this.userName = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtNIC = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbRole = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTel = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnUpdateService = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // userNameVerify
+            // 
+            this.userNameVerify.AutoSize = true;
+            this.userNameVerify.ForeColor = System.Drawing.Color.Red;
+            this.userNameVerify.Location = new System.Drawing.Point(609, 70);
+            this.userNameVerify.Name = "userNameVerify";
+            this.userNameVerify.Size = new System.Drawing.Size(101, 13);
+            this.userNameVerify.TabIndex = 66;
+            this.userNameVerify.Text = "*User Already Exists";
+            this.userNameVerify.Visible = false;
+            // 
+            // roleVerify
+            // 
+            this.roleVerify.AutoSize = true;
+            this.roleVerify.ForeColor = System.Drawing.Color.Red;
+            this.roleVerify.Location = new System.Drawing.Point(343, 178);
+            this.roleVerify.Name = "roleVerify";
+            this.roleVerify.Size = new System.Drawing.Size(11, 13);
+            this.roleVerify.TabIndex = 65;
+            this.roleVerify.Text = "*";
+            this.roleVerify.Visible = false;
+            // 
+            // passwordVerify
+            // 
+            this.passwordVerify.AutoSize = true;
+            this.passwordVerify.ForeColor = System.Drawing.Color.Red;
+            this.passwordVerify.Location = new System.Drawing.Point(698, 179);
+            this.passwordVerify.Name = "passwordVerify";
+            this.passwordVerify.Size = new System.Drawing.Size(11, 13);
+            this.passwordVerify.TabIndex = 64;
+            this.passwordVerify.Text = "*";
+            this.passwordVerify.Visible = false;
+            // 
+            // nicVerify
+            // 
+            this.nicVerify.AutoSize = true;
+            this.nicVerify.ForeColor = System.Drawing.Color.Red;
+            this.nicVerify.Location = new System.Drawing.Point(698, 140);
+            this.nicVerify.Name = "nicVerify";
+            this.nicVerify.Size = new System.Drawing.Size(11, 13);
+            this.nicVerify.TabIndex = 63;
+            this.nicVerify.Text = "*";
+            this.nicVerify.Visible = false;
+            // 
+            // lnameVerify
+            // 
+            this.lnameVerify.AutoSize = true;
+            this.lnameVerify.ForeColor = System.Drawing.Color.Red;
+            this.lnameVerify.Location = new System.Drawing.Point(698, 31);
+            this.lnameVerify.Name = "lnameVerify";
+            this.lnameVerify.Size = new System.Drawing.Size(11, 13);
+            this.lnameVerify.TabIndex = 62;
+            this.lnameVerify.Text = "*";
+            this.lnameVerify.Visible = false;
+            // 
+            // fnameVerify
+            // 
+            this.fnameVerify.AutoSize = true;
+            this.fnameVerify.ForeColor = System.Drawing.Color.Red;
+            this.fnameVerify.Location = new System.Drawing.Point(343, 31);
+            this.fnameVerify.Name = "fnameVerify";
+            this.fnameVerify.Size = new System.Drawing.Size(11, 13);
+            this.fnameVerify.TabIndex = 61;
+            this.fnameVerify.Text = "*";
+            this.fnameVerify.Visible = false;
+            // 
+            // userName
+            // 
+            this.userName.Enabled = false;
+            this.userName.Location = new System.Drawing.Point(108, 67);
+            this.userName.Name = "userName";
+            this.userName.Size = new System.Drawing.Size(489, 20);
+            this.userName.TabIndex = 60;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(27, 66);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 18);
+            this.label8.TabIndex = 59;
+            this.label8.Text = "Username";
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(463, 171);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(229, 20);
+            this.password.TabIndex = 58;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(382, 170);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 18);
+            this.label7.TabIndex = 57;
+            this.label7.Text = "Password";
+            // 
+            // txtNIC
+            // 
+            this.txtNIC.Location = new System.Drawing.Point(463, 136);
+            this.txtNIC.Name = "txtNIC";
+            this.txtNIC.Size = new System.Drawing.Size(229, 20);
+            this.txtNIC.TabIndex = 56;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(424, 138);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(33, 18);
+            this.label6.TabIndex = 55;
+            this.label6.Text = "NIC";
+            // 
+            // cmbRole
+            // 
+            this.cmbRole.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbRole.FormattingEnabled = true;
+            this.cmbRole.Location = new System.Drawing.Point(108, 171);
+            this.cmbRole.Name = "cmbRole";
+            this.cmbRole.Size = new System.Drawing.Size(229, 21);
+            this.cmbRole.TabIndex = 54;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(27, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 18);
+            this.label5.TabIndex = 53;
+            this.label5.Text = "Role";
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(108, 136);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(229, 20);
+            this.txtTel.TabIndex = 52;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(27, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 18);
+            this.label4.TabIndex = 51;
+            this.label4.Text = "Telephone";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(108, 103);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(584, 20);
+            this.txtAddress.TabIndex = 50;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(27, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 18);
+            this.label3.TabIndex = 49;
+            this.label3.Text = "Address";
+            // 
+            // txtLname
+            // 
+            this.txtLname.Location = new System.Drawing.Point(455, 29);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(237, 20);
+            this.txtLname.TabIndex = 48;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(374, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Last Name";
+            // 
+            // txtFname
+            // 
+            this.txtFname.Location = new System.Drawing.Point(108, 27);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(229, 20);
+            this.txtFname.TabIndex = 46;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 18);
+            this.label1.TabIndex = 45;
+            this.label1.Text = "First Name";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(427, 210);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(124, 54);
+            this.btnClear.TabIndex = 44;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnUpdateService
+            // 
+            this.btnUpdateService.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateService.Location = new System.Drawing.Point(568, 210);
+            this.btnUpdateService.Name = "btnUpdateService";
+            this.btnUpdateService.Size = new System.Drawing.Size(124, 54);
+            this.btnUpdateService.TabIndex = 43;
+            this.btnUpdateService.Text = "Update";
+            this.btnUpdateService.UseVisualStyleBackColor = true;
+            this.btnUpdateService.Click += new System.EventHandler(this.btnAddService_Click);
+            // 
+            // editUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 290);
+            this.Controls.Add(this.userNameVerify);
+            this.Controls.Add(this.roleVerify);
+            this.Controls.Add(this.passwordVerify);
+            this.Controls.Add(this.nicVerify);
+            this.Controls.Add(this.lnameVerify);
+            this.Controls.Add(this.fnameVerify);
+            this.Controls.Add(this.userName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtNIC);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.cmbRole);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFname);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdateService);
+            this.Name = "editUsers";
+            this.Text = "Edit Users";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label userNameVerify;
+        private System.Windows.Forms.Label roleVerify;
+        private System.Windows.Forms.Label passwordVerify;
+        private System.Windows.Forms.Label nicVerify;
+        private System.Windows.Forms.Label lnameVerify;
+        private System.Windows.Forms.Label fnameVerify;
+        private System.Windows.Forms.TextBox userName;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNIC;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbRole;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnUpdateService;
+    }
+}
---
+++ AdvancedAutoInitRelease/AdvancedAuto/New folder/AdvancedAuto/uiLayer/editUsers.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using entityLayer;
+using businessLayer;
+
+namespace uiLayer
+{
+    public partial class editUsers : Form
+    {
+        user user;
+        viewUsers currentView;
+        Timer labelTimer;
+
+        public editUsers(user user, viewUsers currentView)
+        {
+            InitializeComponent();
+            this.user = user;
+            this.currentView = currentView;
+            labelTimer = new Timer();
+            fnameVerify.Font = new Font("Calibri", 10);
+            lnameVerify.Font = new Font("Calibri", 10);
+            userNameVerify.Font = new Font("Calibri", 10);
+            nicVerify.Font = new Font("Calibri", 10);
+            roleVerify.Font = new Font("Calibri", 10);
+            passwordVerify.Font = new Font("Calibri", 10);
+            setData();
+        }
+
+        private void setData()
+        {
+            txtFname.Text = user.First_Name;
+            txtLname.Text = user.Last_Name;
+            userName.Text = user.Username;
+            txtAddress.Text = user.Address;
+            txtTel.Text = user.Telephone;
+            txtNIC.Text = user.NIC;
+            password.Text = user.Password;
+            roleDataBindings();
+        }
+
+        private void btnAddService_Click(object sender, EventArgs e)
+        {
+            String fName = ((txtFname.Text).Replace(" ", string.Empty)).Clone().ToString();
+            String lName = ((txtLname.Text).Replace(" ", string.Empty)).Clone().ToString();
+            String nic = ((txtNIC.Text).Replace(" ", string.Empty)).Clone().ToString();
+            String role = ((cmbRole.Text).Replace(" ", string.Empty)).Clone().ToString();
+            String usName = ((userName.Text).Replace(" ", string.Empty)).Clone().ToString();
+
+            if (fName == "")
+            {
+                fnameVerify.Show();
+                hideLabelsTimer();
+            }
+            else if (lName == "")
+            {
+                lnameVerify.Show();
+                hideLabelsTimer();
+            }
+            else if (usName == "")
+            {
+                userNameVerify.Show();
+                hideLabelsTimer();
+            }
+            else if (nic == "")
+            {
+                nicVerify.Show();
+                hideLabelsTimer();
+            }
+            else if (role == "")
+            {
+                roleVerify.Show();
+                hideLabelsTimer();
+            }
+            else if (password.Text == "")
+            {
+                passwordVerify.Show();
+                hideLabelsTimer();
+            }
+            else
+            {
+                String uName = (userName.Text).Clone().ToString();
+                String urole = ((KeyValuePair<String, String>)cmbRole.SelectedItem).Key;
+                String uPword = (password.Text).Clone().ToString();
+                String uFName = (txtFname.Text).Clone().ToString();
+                String uLName = (txtLname.Text).Clone().ToString();
+                String uAddress = (txtAddress.Text).Clone().ToString();
+                String uTel = (txtTel.Text).Clone().ToString();
+                String uLNic = (txtNIC.Text).Clone().ToString();
+                userDataManipulations userDataManipulations = new userDataManipulations();
+                if ((userDataManipulations.validateUserName(uName)))
+                {
+                    user user = new user(uName, urole, uPword, uFName, uLName, uAddress, uTel, uLNic);
+                    Boolean userInsertionFlag = userDataManipulations.updateUser(user);
+                    if (userInsertionFlag)
+                    {
+                        MessageBox.Show("Update Successfull. Data Updated successfully", "Important Note",
+                        MessageBoxButtons.OK);
+                        currentView.bindDataTableData();
+                        this.Dispose();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Update Failed. Check the Updated data again", "Important Note",
+                        MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                    }
+                }
+                else
+                {
+                    userNameVerify.Show();
+                }
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clearForm();
+        }
+
+        private void hideLabelsTimer()
+        {
+            labelTimer.Start();
+
+            labelTimer.Interval = 5000;
+            labelTimer.Tick += (s, e) =>
+            {
+                labelRemover();
+                labelTimer.Stop();
+            };
+        }
+
+        private void labelRemover()
+        {
+            fnameVerify.Hide();
+            lnameVerify.Hide();
+            nicVerify.Hide();
+            roleVerify.Hide();
+            passwordVerify.Hide();
+            userNameVerify.Hide();
+        }
+
+        private void roleDataBindings()
+        {
+            Dictionary<string, string> role = new Dictionary<string, string>();
+            role.Add(user.Role, user.Role);
+            if(!(user.Role == "Admin")){
+                role.Add("Admin", "Admin");
+            }
+            if(!(user.Role == "User")){
+                role.Add("User", "User");
+            }
+            cmbRole.DataSource = new BindingSource(role, null);
+            cmbRole.DisplayMember = "Value";
+            cmbRole.ValueMember = "Key";
+        }
+
+        private void clearForm()
+        {
+            txtFname.Text = "";
+            txtLname.Text = "";
+            txtAddress.Text = "";
+            txtTel.Text = "";
+            txtNIC.Text = "";
+            password.Text = "";
+            cmbRole.Text = "";
+            labelRemover();
+        }
+    }
+}
