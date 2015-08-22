460d776ba44178f4ec52f3a78152457649d3cbc2
manendra <manendraranathunga@gmail.com>
2015-08-23 00:50:16 +0530

    product update and delete completed

---
+++ AdvancedAutoInitRelease/AdvancedAuto/New folder/AdvancedAuto/uiLayer/editProducts.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace uiLayer
+{
+    partial class editProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPrdQty = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.cmbCatVerify = new System.Windows.Forms.Label();
+            this.ppriceVerify = new System.Windows.Forms.Label();
+            this.pnameVerify = new System.Windows.Forms.Label();
+            this.pcodeVerify = new System.Windows.Forms.Label();
+            this.txtProductModel = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtProductMake = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnUpdateProduct = new System.Windows.Forms.Button();
+            this.combProCat = new System.Windows.Forms.ComboBox();
+            this.txtProductPrc = new System.Windows.Forms.TextBox();
+            this.txtProductName = new System.Windows.Forms.TextBox();
+            this.txtProductCode = new System.Windows.Forms.TextBox();
+            this.txtProductDesc = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPrdQty)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtPrdQty
+            // 
+            this.txtPrdQty.Location = new System.Drawing.Point(176, 258);
+            this.txtPrdQty.Maximum = new decimal(new int[] {
+            268435455,
+            1042612833,
+            542101086,
+            0});
+            this.txtPrdQty.Name = "txtPrdQty";
+            this.txtPrdQty.Size = new System.Drawing.Size(355, 20);
+            this.txtPrdQty.TabIndex = 67;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(31, 254);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(118, 18);
+            this.label8.TabIndex = 66;
+            this.label8.Text = "Product Quantity";
+            // 
+            // cmbCatVerify
+            // 
+            this.cmbCatVerify.AutoSize = true;
+            this.cmbCatVerify.ForeColor = System.Drawing.Color.Red;
+            this.cmbCatVerify.Location = new System.Drawing.Point(411, 326);
+            this.cmbCatVerify.Name = "cmbCatVerify";
+            this.cmbCatVerify.Size = new System.Drawing.Size(121, 13);
+            this.cmbCatVerify.TabIndex = 65;
+            this.cmbCatVerify.Text = "*Please Select Category";
+            this.cmbCatVerify.Visible = false;
+            // 
+            // ppriceVerify
+            // 
+            this.ppriceVerify.AutoSize = true;
+            this.ppriceVerify.ForeColor = System.Drawing.Color.Red;
+            this.ppriceVerify.Location = new System.Drawing.Point(538, 297);
+            this.ppriceVerify.Name = "ppriceVerify";
+            this.ppriceVerify.Size = new System.Drawing.Size(11, 13);
+            this.ppriceVerify.TabIndex = 64;
+            this.ppriceVerify.Text = "*";
+            this.ppriceVerify.Visible = false;
+            // 
+            // pnameVerify
+            // 
+            this.pnameVerify.AutoSize = true;
+            this.pnameVerify.ForeColor = System.Drawing.Color.Red;
+            this.pnameVerify.Location = new System.Drawing.Point(538, 74);
+            this.pnameVerify.Name = "pnameVerify";
+            this.pnameVerify.Size = new System.Drawing.Size(11, 13);
+            this.pnameVerify.TabIndex = 63;
+            this.pnameVerify.Text = "*";
+            this.pnameVerify.Visible = false;
+            // 
+            // pcodeVerify
+            // 
+            this.pcodeVerify.AutoSize = true;
+            this.pcodeVerify.ForeColor = System.Drawing.Color.Red;
+            this.pcodeVerify.Location = new System.Drawing.Point(538, 41);
+            this.pcodeVerify.Name = "pcodeVerify";
+            this.pcodeVerify.Size = new System.Drawing.Size(11, 13);
+            this.pcodeVerify.TabIndex = 62;
+            this.pcodeVerify.Text = "*";
+            this.pcodeVerify.Visible = false;
+            // 
+            // txtProductModel
+            // 
+            this.txtProductModel.Location = new System.Drawing.Point(176, 140);
+            this.txtProductModel.Name = "txtProductModel";
+            this.txtProductModel.Size = new System.Drawing.Size(355, 20);
+            this.txtProductModel.TabIndex = 61;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(29, 139);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 18);
+            this.label7.TabIndex = 60;
+            this.label7.Text = "Product Model";
+            // 
+            // txtProductMake
+            // 
+            this.txtProductMake.Location = new System.Drawing.Point(177, 107);
+            this.txtProductMake.Name = "txtProductMake";
+            this.txtProductMake.Size = new System.Drawing.Size(355, 20);
+            this.txtProductMake.TabIndex = 59;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(30, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(101, 18);
+            this.label6.TabIndex = 58;
+            this.label6.Text = "Product Make";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(277, 367);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(124, 54);
+            this.btnClear.TabIndex = 57;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnUpdateProduct
+            // 
+            this.btnUpdateProduct.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateProduct.Location = new System.Drawing.Point(407, 367);
+            this.btnUpdateProduct.Name = "btnUpdateProduct";
+            this.btnUpdateProduct.Size = new System.Drawing.Size(124, 54);
+            this.btnUpdateProduct.TabIndex = 56;
+            this.btnUpdateProduct.Text = "Update";
+            this.btnUpdateProduct.UseVisualStyleBackColor = true;
+            this.btnUpdateProduct.Click += new System.EventHandler(this.btnUpdateProduct_Click);
+            // 
+            // combProCat
+            // 
+            this.combProCat.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.CustomSource;
+            this.combProCat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.combProCat.FormattingEnabled = true;
+            this.combProCat.Location = new System.Drawing.Point(177, 322);
+            this.combProCat.Name = "combProCat";
+            this.combProCat.Size = new System.Drawing.Size(228, 21);
+            this.combProCat.TabIndex = 55;
+            // 
+            // txtProductPrc
+            // 
+            this.txtProductPrc.Location = new System.Drawing.Point(177, 290);
+            this.txtProductPrc.Name = "txtProductPrc";
+            this.txtProductPrc.Size = new System.Drawing.Size(355, 20);
+            this.txtProductPrc.TabIndex = 54;
+            // 
+            // txtProductName
+            // 
+            this.txtProductName.Location = new System.Drawing.Point(177, 71);
+            this.txtProductName.Name = "txtProductName";
+            this.txtProductName.Size = new System.Drawing.Size(355, 20);
+            this.txtProductName.TabIndex = 53;
+            // 
+            // txtProductCode
+            // 
+            this.txtProductCode.Location = new System.Drawing.Point(177, 36);
+            this.txtProductCode.Name = "txtProductCode";
+            this.txtProductCode.Size = new System.Drawing.Size(355, 20);
+            this.txtProductCode.TabIndex = 52;
+            // 
+            // txtProductDesc
+            // 
+            this.txtProductDesc.Location = new System.Drawing.Point(177, 172);
+            this.txtProductDesc.Multiline = true;
+            this.txtProductDesc.Name = "txtProductDesc";
+            this.txtProductDesc.Size = new System.Drawing.Size(355, 68);
+            this.txtProductDesc.TabIndex = 51;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(30, 326);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(124, 18);
+            this.label5.TabIndex = 50;
+            this.label5.Text = "Product Catagory";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(30, 290);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 18);
+            this.label4.TabIndex = 49;
+            this.label4.Text = "Product Price";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(31, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 18);
+            this.label3.TabIndex = 48;
+            this.label3.Text = "Product Description";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 18);
+            this.label2.TabIndex = 47;
+            this.label2.Text = "Product Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(31, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 18);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "Product Code";
+            // 
+            // editProducts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 477);
+            this.Controls.Add(this.txtPrdQty);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.cmbCatVerify);
+            this.Controls.Add(this.ppriceVerify);
+            this.Controls.Add(this.pnameVerify);
+            this.Controls.Add(this.pcodeVerify);
+            this.Controls.Add(this.txtProductModel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtProductMake);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdateProduct);
+            this.Controls.Add(this.combProCat);
+            this.Controls.Add(this.txtProductPrc);
+            this.Controls.Add(this.txtProductName);
+            this.Controls.Add(this.txtProductCode);
+            this.Controls.Add(this.txtProductDesc);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "editProducts";
+            this.Text = "Edit Product";
+            ((System.ComponentModel.ISupportInitialize)(this.txtPrdQty)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown txtPrdQty;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label cmbCatVerify;
+        private System.Windows.Forms.Label ppriceVerify;
+        private System.Windows.Forms.Label pnameVerify;
+        private System.Windows.Forms.Label pcodeVerify;
+        private System.Windows.Forms.TextBox txtProductModel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtProductMake;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnUpdateProduct;
+        private System.Windows.Forms.ComboBox combProCat;
+        private System.Windows.Forms.TextBox txtProductPrc;
+        private System.Windows.Forms.TextBox txtProductName;
+        private System.Windows.Forms.TextBox txtProductCode;
+        private System.Windows.Forms.TextBox txtProductDesc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ AdvancedAutoInitRelease/AdvancedAuto/New folder/AdvancedAuto/uiLayer/editProducts.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using entityLayer;
+using businessLayer;
+
+namespace uiLayer
+{
+    public partial class editProducts : Form
+    {
+        products product;
+        viewProducts currentView;
+        Timer labelTimer;
+
+        public editProducts(products product, viewProducts currentView)
+        {
+            InitializeComponent();
+            this.product = product;
+            this.currentView = currentView;
+            pcodeVerify.Font = new Font("Calibri", 10);
+            pnameVerify.Font = new Font("Calibri", 10);
+            ppriceVerify.Font = new Font("Calibri", 10);
+            cmbCatVerify.Font = new Font("Calibri", 10);
+            labelTimer = new Timer();
+            this.txtProductPrc.KeyPress += new KeyPressEventHandler(txtProductPrc_KeyPress);
+            setData();
+        }
+
+        private void setData()
+        {
+            txtProductCode.Text = product.ProductCode;
+            txtProductName.Text = product.ProductName;
+            txtProductMake.Text = product.ProductMake;
+            txtProductModel.Text = product.ProductModel;
+            txtProductDesc.Text = product.ProductDescription;
+            txtProductPrc.Text = product.ProductPrice.ToString();
+            txtPrdQty.Text = product.Quantity.ToString();
+            categoryDataBindings();
+        }
+
+        private void btnUpdateProduct_Click(object sender, EventArgs e)
+        {
+            String pCode = ((txtProductCode.Text).Replace(" ", string.Empty)).Clone().ToString();
+            String pName = ((txtProductName.Text).Replace(" ", string.Empty)).Clone().ToString();
+            String pPrice = ((txtProductPrc.Text).Replace(" ", string.Empty)).Clone().ToString();
+            String quantity = ((txtPrdQty.Text).Replace(" ", string.Empty)).Clone().ToString();
+            itemCategory pCategory = ((KeyValuePair<itemCategory, string>)combProCat.SelectedItem).Key;
+
+            if (pCode == "")
+            {
+                pcodeVerify.Show();
+                hideLabelsTimer();
+            }
+            else if (pName == "")
+            {
+                pnameVerify.Show();
+                hideLabelsTimer();
+            }
+            else if (pPrice == "")
+            {
+                ppriceVerify.Show();
+                hideLabelsTimer();
+            }
+            else if (quantity == "")
+            {
+                ppriceVerify.Show();
+                hideLabelsTimer();
+            }
+            else if (pCategory.Item_Category_Name == "Default")
+            {
+                cmbCatVerify.Show();
+                hideLabelsTimer();
+            }
+            else
+            {
+                string productCode = txtProductCode.Text;
+                string productName = txtProductName.Text;
+                string productMake = txtProductMake.Text;
+                string productModel = txtProductModel.Text;
+                string productDescription = txtProductDesc.Text;
+                string productPrice = txtProductPrc.Text;
+                int qty = (int)txtPrdQty.Value;
+                itemCategory itemCat = ((KeyValuePair<itemCategory, string>)combProCat.SelectedItem).Key;
+
+
+                products product1 = new products(product.ProductID, productCode, productName, productMake, productModel,
+                    productDescription, decimal.Parse(productPrice), itemCat, qty);
+
+                stockDataManipulations stockDataManipulations = new stockDataManipulations();
+                Boolean stockDeletion = stockDataManipulations.updateProduct(product1);
+
+                if (stockDeletion)
+                {
+                    MessageBox.Show("Update Successfull. Data updated successfully", "Important Note",
+                    MessageBoxButtons.OK);
+                    currentView.bindDataTableData();
+                    this.Dispose();
+                }
+                else
+                {
+                    MessageBox.Show("Update Failed. Error occured while Updating data", "Important Note",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button1);
+                }
+            }
+        }
+
+        private void txtProductPrc_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void clearForm()
+        {
+            txtProductCode.Text = "";
+            txtProductName.Text = "";
+            txtProductMake.Text = "";
+            txtProductModel.Text = "";
+            txtProductDesc.Text = "";
+            txtProductPrc.Text = "";
+            txtPrdQty.Text = "";
+            labelRemover();
+        }
+
+        private void hideLabelsTimer()
+        {
+            labelTimer.Start();
+
+            labelTimer.Interval = 5000;
+            labelTimer.Tick += (s, e) =>
+            {
+                labelRemover();
+                labelTimer.Stop();
+            };
+        }
+
+        private void labelRemover()
+        {
+            pcodeVerify.Hide();
+            pnameVerify.Hide();
+            ppriceVerify.Hide();
+            cmbCatVerify.Hide();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clearForm();
+        }
+
+        private void categoryDataBindings()
+        {
+            stockDataManipulations stockDataManipulation = new stockDataManipulations();
+            List<itemCategory> categories = stockDataManipulation.getAllCategories();
+            Dictionary<itemCategory, string> productCategory = new Dictionary<itemCategory, string>();
+            productCategory.Add(product.ProductCateogery, product.ProductCateogery.Item_Category_Name);
+            foreach (itemCategory category in categories)
+            {
+                productCategory.Add(category, category.Item_Category_Name);
+            }
+            combProCat.DataSource = new BindingSource(productCategory, null);
+            combProCat.DisplayMember = "Value";
+            combProCat.ValueMember = "Key";
+        }
+    }
+}
